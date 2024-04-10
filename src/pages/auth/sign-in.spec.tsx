dc1710745dca0dfea275348375ab99c9b27268de
Wellington Henrique <wellingtonhlc@gmail.com>
2024-04-09 21:47:18 -0300

    Testes e encerramento: Teste unitário - Sign In

---
+++ src/pages/auth/sign-in.spec.tsx
@@ -0,0 +1,34 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+
+import { queryCliente } from '@/lib/react-query'
+
+import { SignIn } from './sign-in'
+
+describe('SignIn', () => {
+  it('should set email input value if email is present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => {
+        return (
+          <HelmetProvider>
+            <MemoryRouter
+              initialEntries={['/sign-in?email=johndoe@example.com']}
+            >
+              <QueryClientProvider client={queryCliente}>
+                {children}
+              </QueryClientProvider>
+            </MemoryRouter>
+          </HelmetProvider>
+        )
+      },
+    })
+
+    // wrapper.debug() // renderiza o componente
+
+    const emailInput = wrapper.getByLabelText('Seu e-mail') as HTMLInputElement
+
+    expect(emailInput.value).toEqual('johndoe@example.com')
+  })
+})
